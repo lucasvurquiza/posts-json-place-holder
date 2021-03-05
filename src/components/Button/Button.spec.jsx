2954d8b1fb6c92e5718b39eb852dd2d91ce9eb7b
Lucas Urquiza <urquizaxd@gmail.com>
2021-03-05 16:03:45 -0300

    Adicionando os teste

---
+++ src/components/Button/Button.spec.jsx
@@ -0,0 +1,32 @@
+const { render, screen, fireEvent } = require("@testing-library/react");
+import userEvent from '@testing-library/user-event';
+import { Button } from '.';
+
+describe('<Button />', () => {
+    it('should render the button with the text "Load more"', () => {
+        render(<Button text="Load more" />);
+        expect.assertions(1);
+        const button = screen.getByRole('button', {name: /load more/i });
+        expect(button).toBeInTheDocument();
+    });
+
+    it('should call function on button click', () => {
+        const fn = jest.fn();
+        render(<Button text="Load more" onClick={fn}/>);
+        const button = screen.getByRole('button', {name: /load more/i });
+        userEvent.click(button);
+        expect(fn).toHaveBeenCalledTimes(1);        
+    });
+
+    it('should be disable when disabled is true', () => {
+        render(<Button text="Load more" disabled={true}/>);
+        const button = screen.getByRole('button', {name: /load more/i });
+        expect(button).toBeDisabled();        
+    });
+
+    it('should match snapshot', () => {
+        const fn = jest.fn();
+        const {container} = render(<Button text="Load more" disabled={false} onClick={fn}/>);
+        expect(container.firstChild).toMatchSnapshot();        
+    });
+});
--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -5,7 +5,7 @@ export const PostCard = ({ title, cover, body, id }) => {
         <div className="post">
             <img src={cover} alt={title} />
             <div className="post-content">
-                <h2>{title}</h2>
+                <h2>{title} {id}</h2>
                 <h4>{body}</h4>
             </div>
         </div>
